8bd9e9a5628cdc9805dcc416b799ead968053a79
Gordonmajoni <gordonmajoni95@gmail.com>
2022-05-29 22:00:10 +0200

    change

---
+++ chamber/script.js
@@ -0,0 +1,13 @@
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+
+document.querySelector('#©').textContent = new Date(Date.now()).getFullYear();
