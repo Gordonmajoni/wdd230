3212405c8c8600fb975340e85774c6a13d1c9f60
Gordonmajoni <gordonmajoni95@gmail.com>
2022-06-04 16:47:00 +0200

    new

--- lesson 7/lazyload.js
+++ lesson 7/lazyload.js
@@ -1,25 +1,25 @@
-const images = document.querySelectorAll("[data-src]");
-function preloadImage(src){
-    const src = img.getAttribute("data-src");
-    if(!src){
-        return;
-    }
-    img.src = src;
-}
-const imgOptions = {
-    threshold : 0,
-    rootMargin: "0px 0px -500px 0px"
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
-
-const imgObsever = new IntersectionObserver((entries,imgObsever)=>{
-    entries.forEach(entry =>{
-        if(!entry.isIntersecting){
-            return;
-        }else{
-            preloadImage(entry.target);
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
         }
+      });
     });
-},imgOptions);
-images.forEach(img =>{
-    imgObsever.observe(img);
-});
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+  
