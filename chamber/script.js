9b73492acac09b0102994b03de82369762d295ba
Gordonmajoni <gordonmajoni95@gmail.com>
2022-06-07 05:25:00 +0200

    changes

--- chamber/script.js
+++ chamber/script.js
@@ -11,3 +11,35 @@ datefield.innerHTML = `<em>${fulldate}</em>`;
 
 
 document.querySelector('#©').textContent = new Date(Date.now()).getFullYear();
+//this will get all images with data-src attribute
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+//this will determine when the img is loaded//
+const imgOptions = {
+    threshold:1,
+    rootMargin: "0px 0px -500px 0px"
+};
+//when the other image is finished loading
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+  
