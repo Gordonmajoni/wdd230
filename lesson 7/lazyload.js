3c979b5d59e15da5fd5e61ae5ec989a96689f494
Gordonmajoni <gordonmajoni95@gmail.com>
2022-06-11 20:52:24 +0200

    new add

--- lesson 7/lazyload.js
+++ lesson 7/lazyload.js
@@ -1,6 +1,6 @@
 
 //this will get all images with data-src attribute
-let imagesToLoad = document.querySelectorAll('[data-src]');
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
@@ -9,8 +9,8 @@ const loadImages = (image) => {
 };
 //this will determine when the img is loaded//
 const imgOptions = {
-    threshold:1,
-    rootMargin: "0px 0px 500px 0px"
+    threshold:0,
+    rootMargin: "0px 0px 70px 0px"
 };
 //when the other image is finished loading
 if('IntersectionObserver' in window) {
