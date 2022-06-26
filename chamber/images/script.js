4456b34bd811a6cbbf5107b556f75549c39adb5a
Gordonmajoni <gordonmajoni95@gmail.com>
2022-06-26 06:49:32 +0200

    files

--- chamber/images/script.js
+++ chamber/images/script.js
@@ -10,7 +10,7 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
 
-document.querySelector('#©').textContent = new Date(Date.now()).getFullYear();
+//document.querySelector('#©').textContent = new Date(Date.now()).getFullYear();
 //this will get all images with data-src attribute
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
@@ -43,4 +43,37 @@ if('IntersectionObserver' in window) {
     });
   }
  //--------join.js--------------------//
- 
+ //----------weather api---------------//
+ // select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const URL = `https://api.openweathermap.org/data/2.5/weather?q=Harare,ZW&units=Imperial&appid=eb8038495bcd010f5e861180e3becf9f`;
+
+async function apiFetch(apiURL) {
+  try {
+    const response = await fetch(URL);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else  {
+      throw Error(await response.text());
+      }
+    } catch (error) {
+       //console.log(error);
+    }
+}
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+
+}
+apiFetch();
+  
