b626c3d58739401725565462991a8f169e7bbc43
Gordonmajoni <gordonmajoni95@gmail.com>
2023-01-14 16:36:49 +0200

    js

--- script/date.js
+++ script/date.js
@@ -4,7 +4,8 @@ const date1 = document.querySelector("#date1");
 const monthName = months[d.getMonth()];
 
 date1.innerHTML = new Date().getFullYear();
-
+date1.innerHTML = new Date().getDay();
+date1.innerHTML = new Date().getMonth();
 
 // Getting the last modified date
 
