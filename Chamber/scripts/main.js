0ce620eedd9f251a7407a44f317ce8f5c465ab12
Gordonmajoni <gordonmajoni95@gmail.com>
2023-01-27 21:58:04 +0200

    date

---
+++ Chamber/scripts/main.js
@@ -0,0 +1,11 @@
+const date = new Date();
+const [month, day, year] = [
+  date.getMonth(),
+  date.getDate(),
+  date.getFullYear(),
+];
+const [hour, minutes, seconds] = [
+  date.getHours(),
+  date.getMinutes(),
+  date.getSeconds(),
+];
