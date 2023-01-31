c0400b810044dfd03f11af83b3c174183b8f5604
Gordonmajoni <gordonmajoni95@gmail.com>
2023-01-31 21:51:34 +0200

    css

--- Chamber/scripts/main.js
+++ Chamber/scripts/main.js
@@ -1,3 +1,6 @@
-document.querySelector(
-  "#date"
-).textContent = `${document.date}`;
+var myDate =new Date();
+  month =myDate.getMonth(),
+  date = myDate.getDate(),
+  day =myDate.getDay();
+
+  
