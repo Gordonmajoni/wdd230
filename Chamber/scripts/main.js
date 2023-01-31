198af2688e8925b47097c5ffd89f4566bc5bdff5
Gordonmajoni <gordonmajoni95@gmail.com>
2023-01-31 19:15:28 +0200

    js

--- Chamber/scripts/main.js
+++ Chamber/scripts/main.js
@@ -1,11 +1,3 @@
-
-const [month, day, year] = [
-  date.getMonth(),
-  date.getDate(),
-  date.getFullYear(),
-];
-const [hour, minutes, seconds] = [
-  date.getHours(),
-  date.getMinutes(),
-  date.getSeconds(),
-];
+document.querySelector(
+  "#date"
+).textContent = `${document.date}`;
