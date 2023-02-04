bf335882fb22db02c2fa2507ee6696d795114874
Gordonmajoni <gordonmajoni95@gmail.com>
2023-02-04 21:13:53 +0200

    js

--- lesson 5/script/bom.js
+++ lesson 5/script/bom.js
@@ -1,4 +1,6 @@
 const input = document.querySelector("#favchap");
+
 const button = document.querySelector("#bttn");
- button.addEventListener('click', function() => {input.textContent ="moses 1"});
-const list = document.querySelector("#list");
+ button.addEventListener('click', function() {input.textContent ="moses 1"});
+
+ const list = document.querySelector("#list");
